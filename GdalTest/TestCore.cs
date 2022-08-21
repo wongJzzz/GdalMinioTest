a3390e97369e8be4cec813ac52dc5a1da3cfaf90
wongjzzz <wong105012@163.com>
2022-08-22 02:18:49 +0800

    get CMD

--- GdalTest/TestCore.cs
+++ GdalTest/TestCore.cs
@@ -16,19 +16,21 @@ namespace GdalTest
 
             Gdal.AllRegister();
 
-            Gdal.SetConfigOption("AWS_HTTPS", "NO");
+            Gdal.SetConfigOption("AWS_HTTPS", "NO"); 
             Gdal.SetConfigOption("GDAL_DISABLE_READDIR_ON_OPEN", "YES");
             Gdal.SetConfigOption("AWS_VIRTUAL_HOSTING", "FALSE");
-            Gdal.SetConfigOption("AWS_S3_ENDPOINT", "localhost:9000");
-            Gdal.SetConfigOption("AWS_SECRET_ACCESS_KEY", "minioadmin");
-            Gdal.SetConfigOption("AWS_ACCESS_KEY_ID", "minioadmin");
+            Gdal.SetConfigOption("AWS_S3_ENDPOINT", "localhost:9000"); // 地址
+            Gdal.SetConfigOption("AWS_SECRET_ACCESS_KEY", "minioadmin"); // 密码
+            Gdal.SetConfigOption("AWS_ACCESS_KEY_ID", "minioadmin"); // 账号
             
-
-
             string filePath = @"/vsis3/testbucket/dem.tif";
             Dataset ds = (Dataset)Gdal.Open(filePath, Access.GA_ReadOnly);
-            Console.WriteLine(ds);
+            string dsPrj = ds.GetProjection();
+            double[] trans = new double[6];
+            ds.GetGeoTransform(trans);
 
+
+            Console.WriteLine("Done");
         }
     }
 }
